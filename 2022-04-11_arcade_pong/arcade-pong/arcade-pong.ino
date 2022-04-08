17bc4cc95b0430e65568d6d691f5f842ee306dde
fremsoft <41731090+fremsoft@users.noreply.github.com>
2022-04-08 12:31:44 +0200

    draft

---
+++ 2022-04-11_arcade_pong/arcade-pong/arcade-pong.ino
@@ -0,0 +1,25 @@
+/* 
+ * Questo programma per Arduino UNO serve a realizzare
+ * il gioco del PONG, utilizzando due Joystick analogici
+ * per muovere le palette di destra e di sinistra, 
+ * utilizzando come schermo un display DOT-MATRIX  8x8
+ * a LED basato su controller MAX7219.
+ * 
+ * Si tratta di un videogame dimostrativo a scopo didattico.
+ * 
+ * L'idea di creare questo gioco è sorta vedendo un video 
+ * su Facebook in cui veniva ritratto un videogame fatto 
+ * come descritto sopra.
+ * 
+ * Visualizza l'esperienza di laboratorio completa: 
+ * https://youtu.be/...
+ * 
+ */
+
+void setup() {
+  Serial.begin(9600);
+}
+
+
+void loop() {
+}
---
+++ 2022-04-11_arcade_pong/max7219-emulator/max7219-emulator.ino
@@ -0,0 +1,19 @@
+/* 
+ * Questo programma per Arduino UNO serve a emulare 
+ * un controller MAX7219, utilizzato per accendere 
+ * i LED di display molto grandi, in modo multiplato.
+ * 
+ * I comandi provengono dalla linea SPI da un altro 
+ * Arduino UNO.
+ * 
+ * Visualizza l'esperienza di laboratorio completa: 
+ * https://youtu.be/...
+ */
+
+void setup() {
+  Serial.begin(9600);
+}
+
+
+void loop() {
+}
