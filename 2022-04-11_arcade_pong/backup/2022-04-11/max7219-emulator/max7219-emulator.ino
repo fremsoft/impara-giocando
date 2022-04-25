ebebc28e45867329ddeb56b86b3b14bd7db93b8f
fremsoft <41731090+fremsoft@users.noreply.github.com>
2022-04-26 00:42:04 +0200

    Laboratorio del 25 aprile 2022

--- 2022-04-11_arcade_pong/backup/2022-04-11/max7219-emulator/max7219-emulator.ino
+++ 2022-04-11_arcade_pong/backup/2022-04-11/max7219-emulator/max7219-emulator.ino
@@ -7,8 +7,7 @@
  * Arduino UNO.
  * 
  * Visualizza l'esperienza di laboratorio completa: 
- * 1) https://youtu.be/Ptr_G2O8gbQ
- * 2) https://youtu.be/bSmhxYcj3xs
+ * https://youtu.be/Ptr_G2O8gbQ
  */
 
 #define  R_1     9
@@ -29,9 +28,11 @@
 #define  C_7    15
 #define  C_8    14
 
+
 #define  DIN    12
 #define  CLK    3
 #define  LOAD   2
+
 
 char buffer[8];
 volatile unsigned int registro_16bit;
@@ -44,19 +45,6 @@ void caricamentoRegistroScorrimento() {
   }
 }
 
-void caricamentoRegistroNelBuffer() {
-  int n;
-  int val;
-  unsigned int raw = registro_16bit;
-  
-  n =   (raw >> 8) & 0x0F;
-  val = (raw     ) & 0xFF;
-  
-  if ((n>=1) && (n<=8)) {
-    /* siamo nella zona delle righe */
-    buffer[ n - 1 ] = val;
-  }
-}
 
 void setup() {
   
@@ -97,27 +85,15 @@ void setup() {
   digitalWrite (C_8, LOW);
   
   registro_16bit = 0;
-  buffer[0] = 0;
-  buffer[1] = 0;
-  buffer[2] = 0;
-  buffer[3] = 0;
-  buffer[4] = 0;
-  buffer[5] = 0;
-  buffer[6] = 0;
-  buffer[7] = 0;  
   
   pinMode(DIN, INPUT);
   pinMode(CLK, INPUT);
   pinMode(LOAD, INPUT);
-  
   attachInterrupt(digitalPinToInterrupt(CLK), 
                   caricamentoRegistroScorrimento, 
                   RISING);
-
-  attachInterrupt(digitalPinToInterrupt(LOAD), 
-                  caricamentoRegistroNelBuffer, 
-                  RISING);
 }
+
 
 void frullaTutteLeColonne() {
 
@@ -155,61 +131,38 @@ void frullaTutteLeColonne() {
   
 }
 
-void accensioneRiga(int n) {
-  /* n è il numero della riga che vogliamo accendere        */
-  /* dove 0 è la prima riga in alto e 7 è l'ultima in basso */
-  char b = buffer[n];
-  
-  digitalWrite (C_1, bitRead(b, 7) );
-  digitalWrite (C_2, bitRead(b, 6) );
-  digitalWrite (C_3, bitRead(b, 5) );
-  digitalWrite (C_4, bitRead(b, 4) );
-  digitalWrite (C_5, bitRead(b, 3) );
-  digitalWrite (C_6, bitRead(b, 2) );
-  digitalWrite (C_7, bitRead(b, 1) );
-  digitalWrite (C_8, bitRead(b, 0) );
-}
-
 void loop() {
   
-  accensioneRiga(0);
   digitalWrite (R_1, HIGH);
-  delay(10);
+  frullaTutteLeColonne();
   digitalWrite (R_1, LOW);
-       
-  accensioneRiga(1);
+
   digitalWrite (R_2, HIGH);
-  delay(10);
+  frullaTutteLeColonne();
   digitalWrite (R_2, LOW);
 
-  accensioneRiga(2);
   digitalWrite (R_3, HIGH);
-  delay(10);
+  frullaTutteLeColonne();
   digitalWrite (R_3, LOW);
 
-  accensioneRiga(3);
   digitalWrite (R_4, HIGH);
-  delay(10);
+  frullaTutteLeColonne();
   digitalWrite (R_4, LOW);
 
-  accensioneRiga(4);
   digitalWrite (R_5, HIGH);
-  delay(10);
+  frullaTutteLeColonne();
   digitalWrite (R_5, LOW);
 
-  accensioneRiga(5);
   digitalWrite (R_6, HIGH);
-  delay(10);
+  frullaTutteLeColonne();
   digitalWrite (R_6, LOW);
 
-  accensioneRiga(6);
   digitalWrite (R_7, HIGH);
-  delay(10);
+  frullaTutteLeColonne();
   digitalWrite (R_7, LOW);
 
-  accensioneRiga(7);
   digitalWrite (R_8, HIGH);
-  delay(10);
+  frullaTutteLeColonne();
   digitalWrite (R_8, LOW);
-    
 }
+  
